d2a9ca094a1feb34c938cfd55bd9b62851931938
ArtemDobro <ArtemDobrohotov2002@gmail.com>
2022-05-11 17:33:11 +0200

    delete handling and ferry

--- handling/src/by/epam/dobrohotov/composite/TextComponent.java
+++
@@ -1,8 +0,0 @@
-package by.epam.dobrohotov.composite;
-
-public abstract class TextComponent {
-
-  public TextComponent getChild(int i) {
-    throw new UnsupportedOperationException();
-  }
-}
--- handling/src/by/epam/dobrohotov/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package by.epam.dobrohotov.utils;
-
-public final class Constants {
-  public static final String PARAGRAPH_GENEREX = "\r\n\t";
-  public static final String SENTENCE_GENEREX = "\\.";
-  public static final String WORD_GENEREX = " ";
-}
